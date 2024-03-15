db1a9d9f1d4f357b8f21740d867410a05da98f5a
BabaYaga840 <104863253+BabaYaga840@users.noreply.github.com>
2024-03-16 03:17:37 +0530

    fix yaml

--- run.py
+++ run.py
@@ -11,12 +11,15 @@ import wandb
 from utils.q_net import Q_network
 from utils.Replay import ReplayMemory
 from models.DQN import DQN
+from models.dDQN import dDQN
+
 
 with open("config.yaml") as f:
     cfg=yaml.load(f, Loader=yaml.FullLoader)
 
 # PARAMETERS
 environ=cfg["env"]
+algo=cfg["algo"]
 
 cfgm=cfg["model"]
 num_iterations = cfgm["num_iterations"]
@@ -64,6 +67,40 @@ def dqn(n_timesteps=num_iterations, learning_rate=learning_rate, gamma=gamma, po
         env.close()
     return rewards
 
+def ddqn(n_timesteps=num_iterations, learning_rate=learning_rate, gamma=gamma, policy="egreedy", epsilon=epsilon, environ=environ):
+    global re
+    env = gym.make(environ, max_episode_steps=1000)
+    eval_env = gym.make(environ)
+    rewards=[]
+    agent = dDQN(replay_buffer_max_length,
+                        env.action_space.n, 
+                        env.observation_space.shape[0],
+                        epsilon=epsilon, 
+                        gamma=gamma,
+                        lr=learning_rate)
+    observation = env.reset(seed=42)
+    for iteration in range(n_timesteps):
+        state = env.reset()
+        done = False
+        total_reward=0
+        while not done:
+          ch=np.random.randint(2)
+          action = agent.get_action(state,ch).argmax()
+          next_state, reward, done, truncated = env.step(int(action))
+          total_reward=total_reward+reward
+          agent.add_to_buffer(state,action,reward,next_state,done,ch)
+          if len(agent.replay_buffer)>=batch_size:
+            re=agent.replay_buffer
+            agent.replay(batch_size)
+          if done:
+            rewards.append(total_reward)
+            wandb.log({"total_reward": total_reward})
+            total_reward=0
+            break
+          state=next_state
+        env.close()
+    return rewards
+
 if __name__ == "__main__":
     wandb.init(
     # set the wandb project where this run will be logged
@@ -81,7 +118,10 @@ if __name__ == "__main__":
     "epsilon": epsilon
     }
     )
-    rewards=dqn()
+    if algo=="dqn":
+        rewards=dqn()
+    else:
+        rewards=ddqn()
     wandb.finish()
 
 
